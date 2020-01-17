0a6d01640644e33cb3ce19447b36adbee8b603bd
William Blase <william.blase@gmail.com>
2020-01-17 04:04:00 -0800

    Add explanation

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ class App extends Component {
       <div className="App">
         <h1>Conway's Game of Life</h1>
         <Game />
+      <p>
+          "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
+The game is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves." - Wikipedia
+      </p>
       </div>
     );
   }
